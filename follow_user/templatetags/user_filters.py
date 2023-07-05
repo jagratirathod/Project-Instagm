a86a54ac29de68769074a23c05a9c3e86b206cb7
jagrati <jagrati@gmail.com>
2023-07-05 15:28:09 +0530

    show the count of follower and following of user

--- follow_user/templatetags/user_filters.py
+++ follow_user/templatetags/user_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from follow_user.models import SendRequest
 
 register = template.Library()
 
@@ -8,3 +9,11 @@ def is_request_sent(user, sender):
     return user.send.filter(sender=sender).exists()
 
 
+@register.filter
+def get_no_of_following(request):
+    return SendRequest.objects.filter(user__email=request.user,status="Following").count()
+
+
+@register.filter
+def get_no_of_follower(request):
+    return SendRequest.objects.filter(user__email=request.user,status="Accept").count()
